a8058d65ab0cd0247a347daf11ffaefe63367ff8
Alejandro Peña de Alba <alejandro.pena@gymondo.de>
2020-12-06 15:07:43 +0100

    Adding credentials

--- jenkins/push/push.sh
+++ jenkins/push/push.sh
@@ -8,7 +8,7 @@ echo ""
 IMAGE="maven-project"
 
 echo "*** Docker Login ***"
-docker login
+docker login -u mrpecker -p A73jandr0.,!
 echo "*** Tagging Image ***"
 docker tag $IMAGE:$BUILD_TAG mrpecker/$IMAGE:$BUILD_TAG
 echo "*** Pushing Image ***"
