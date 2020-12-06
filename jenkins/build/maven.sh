018f23bdc08154ba04f416638a15d8660ec8e9e0
Alejandro Peña de Alba <alejandro.pena@gymondo.de>
2020-12-06 15:18:35 +0100

    Fix workspace directory

--- jenkins/build/maven.sh
+++ jenkins/build/maven.sh
@@ -4,7 +4,9 @@ echo "****************************************"
 echo "************ Building JAR **************"
 echo "****************************************"
 
-docker run --rm -v $PWD/java-app:/app -v /Users/alejandro.pena/.m2/:/root/.m2/ -w /app maven mvn "$@"
+WORKSPACE=/Users/alejandro.pena/Services/jenkins/jenkins-data/jenkins_home/workspace/pipeline-docker-maven
+
+docker run --rm -v $WORKSPACE/java-app:/app -v /Users/alejandro.pena/.m2/:/root/.m2/ -w /app maven mvn "$@"
 
 echo "****************************************"
 echo "******** Finished Building JAR *********"
